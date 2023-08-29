798cfccacdb145389dfc23064aa1fa5f167de0e4
Avik-creator <avikm744@gmail.com>
2023-08-29 12:30:45 +0530

    Added Changes

--- app/components/UserTag.tsx
+++ app/components/UserTag.tsx
@@ -3,25 +3,29 @@ import React from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import Image from 'next/image';
 
-function UserTag({user}) {
-  console.log("User",user)
+interface User {
+  name: string;
+  email: string;
+  image: string;
+}
+
+function UserTag({ user }: { user: User }) {
+  console.log("User", user)
   return (
     <div className=''>
-       {user?
-       <div className='flex gap-3 
-       items-center'>
-       <Image src={user.image} 
-       alt='userImage'
-       width={45}
-       height={45}
-       className='rounded-full'/>
-       <div>
-        <h2 className='text-[14px] font-medium'>{user.name}</h2>
-        <h2 className='text-[12px]'>{user.email}</h2>
-
+      {user ?
+        <div className='flex gap-3 items-center'>
+          <Image src={user.image}
+            alt='userImage'
+            width={45}
+            height={45}
+            className='rounded-full' />
+          <div>
+            <h2 className='text-[14px] font-medium'>{user.name}</h2>
+            <h2 className='text-[12px]'>{user.email}</h2>
+          </div>
         </div>
-       </div>
-       :null}
+        : null}
     </div>
   )
 }
