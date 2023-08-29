78c49d9f87186ea1d94f48dcfff693267eb94034
Avik-creator <avikm744@gmail.com>
2023-08-29 12:42:12 +0530

    Added Changes

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,8 +1,14 @@
 "use client"
-import React from 'react'
+import React, {ReactNode} from 'react'
 import { SessionProvider } from "next-auth/react"
 
-function Provider({children}) {
+interface ProviderProps {
+  children: ReactNode;
+}
+
+function Provider({ children }: ProviderProps) {
+
+  console.log(children)
   return (
     <SessionProvider >
         {children}
